20559094696d4612af0311f610e3de8e8dd1b231
Nathkit_Nae <nathkit101@gmail.com>
2023-08-31 11:49:28 +0700

    second commit

---
+++ server/utils/db.js
@@ -0,0 +1,9 @@
+// โค้ดนี้อยู่ในไฟล์ server/utils/db.js
+import * as pg from "pg";
+const { Pool } = pg.default;
+
+const pool = new Pool({
+    connectionString: "postgresql://postgres:vN4Qg4n-ypk@pLS@db.wjxguyrdfqbtwsetylfq.supabase.co:5432/postgres",
+});
+
+export { pool };

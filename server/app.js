794d160dfb6d2af32f97b5cad31e8a3b603bfbd4
Babe <panyawit.sea@gmail.com>
2023-08-31 13:19:57 +0700

    fix app.js add port

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+async function init() {
+  const app = express();
+  const port = 4000;
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.get("*", (req, res) => {
+    res.status(404).send("Not found");
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+init();

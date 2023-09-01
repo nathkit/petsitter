e586c9cfbde2cc7858e11e4c8f5755b2babb0d45
Nathkit_Nae <nathkit101@gmail.com>
2023-09-02 00:13:22 +0700

    feat:Create utility button and Icon not yet finish the icon

--- client/src/components/systemdesign/Button.jsx
+++ client/src/components/systemdesign/Button.jsx
@@ -1,8 +1,49 @@
-function Button(props) {
+export function ButtonPrimary(props) {
   return (
-    <button className="inline-flex justify-center items-center py-3 px-6 rounded-[99px]">
+    <button
+      className={`inline-flex justify-center items-center py-3 px-6 rounded-full gap-2 border-none
+       bg-orange-500 text-etc-white
+        hover:bg-orange-400 active:bg-orange-600
+         disabled:bg-gray-200 disabled:text-gray-300`}
+      style={{ width: props.width, height: "24px" }}>
       {props.content}
     </button>
   );
 }
-export default Button;
+export function ButtonSecondary(props) {
+  return (
+    <button
+      className={`inline-flex justify-center items-center py-3 px-6 rounded-full gap-2 border-none
+        bg-orange-100 text-orange-500
+        hover:text-orange-400 active:text-orange-600
+         disabled:bg-gray-200 disabled:text-gray-100`}
+      style={{ width: props.width, height: "24px" }}>
+      {props.content}
+    </button>
+  );
+}
+export function ButtonGhost(props) {
+  return (
+    <button
+      className={`inline-flex justify-center items-center py-1 px-0.5 rounded-full gap-1 border-none
+      bg-transparent text-orange-500
+        hover:text-orange-400 active:text-orange-600
+         disabled:text-gray-200`}
+      style={{ width: props.width, height: "24px" }}>
+      {props.content}
+    </button>
+  );
+}
+export function ButtonSocial(props) {
+  return (
+    <button
+      className={`inline-flex justify-center items-center py-3 px-6 rounded-full gap-2 border-none
+        bg-gray-100 text-gray-600
+       hover:text-gray-400 active:text-gray-600
+         disabled:text-gray-600`}
+      style={{ width: props.width, height: "24px" }}>
+      {props.icon ? <props.icon /> : null}
+      {props.content}
+    </button>
+  );
+}
--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-
+import { ExclamationCircleIcon } from "../components/systemdesign/Icons";
+import {
+  ButtonPrimary,
+  ButtonSecondary,
+  ButtonGhost,
+  ButtonSocial,
+} from "../components/systemdesign/Button";
 function HomePage() {
   return (
     <>
-      <h1>11111</h1>
+      <ButtonPrimary content="ButtonPrimary" width="200px" />
+      <ButtonSecondary content="ButtonSecondary" width="200px" />
+      <ButtonGhost content="ButtonGhost" />
+      <ButtonSocial
+        content="ButtonSocial"
+        width="400px"
+        icon={ExclamationCircleIcon}
+      />
+      <div></div>
     </>
   );
 }

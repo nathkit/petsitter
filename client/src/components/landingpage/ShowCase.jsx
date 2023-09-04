ea13fd99fa1b626fea034d86b707d079b98d95fd
Nathkit_Nae <nathkit101@gmail.com>
2023-09-04 12:37:32 +0700

    feat:add component showcase

---
+++ client/src/components/landingpage/ShowCase.jsx
@@ -0,0 +1,40 @@
+function ShowCase() {
+  return (
+    <div className="w-full p-20">
+      <p>"Your Pets, Our Priority: Perfect Care, Anytime, Anywhere."</p>
+      <div>
+        <div>
+          <div>
+            <p>Boarding</p>
+            <p>
+              Your pets stay overnight in your sitter’s home. They’ll be treated
+              like part of the family in a comfortable environment.
+            </p>
+          </div>
+          <div>
+            <p>House Sitting</p>
+            <p>
+              Your sitter takes care of your pets and your home. Your pets will
+              get all the attention they need without leaving home.
+            </p>
+          </div>
+          <div>
+            <p>Dog Walking</p>
+            <p>
+              Your dog gets a walk around your neighborhood. Perfect for busy
+              days and dogs with extra energy to burn.
+            </p>
+          </div>
+          <div>
+            <p>Drop-In Visits</p>
+            <p>
+              Your sitter drops by your home to play with your pets, offer food,
+              and give potty breaks or clean the litter box.
+            </p>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+}

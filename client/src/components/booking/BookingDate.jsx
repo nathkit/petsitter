6129c9e9ad35195ad68f6c7d20b97586e8c0aea9
Ly <korakot.jarunongkran>
2023-09-04 14:59:24 +0700

    feat: navigate to booking page

--- client/src/components/booking/BookingDate.jsx
+++ client/src/components/booking/BookingDate.jsx
@@ -1,19 +1,21 @@
 import { CalendarIcon, ClockIcon } from "../systemdesign/Icons";
 import { ButtonPrimary } from "../systemdesign/Button";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function BookingDate() {
   const [startTime, setstartTime] = useState("");
   const [endTime, setEndTime] = useState("");
   const [date, setDate] = useState("");
+  const navigate = useNavigate();
 
   console.log(`booking on date ${date} time ${startTime} to ${endTime}`);
 
   return (
     <>
       {" "}
-      <button className="btn" onClick={() => window.my_modal_3.showModal()}>
-        open modal
+      <button onClick={() => window.my_modal_3.showModal()}>
+        <ButtonPrimary content="Book Now" width=" 250px" />
       </button>
       <dialog id="my_modal_3" className="modal ">
         <form method="dialog" className="modal-box max-w-[560px] px-10 py-0">
@@ -57,7 +59,9 @@ function BookingDate() {
                 />
               </div>
             </div>
-            <ButtonPrimary width={"100%"} content={"Continue"} />
+            <div onClick={() => navigate("/booking")}>
+              <ButtonPrimary width={"100%"} content={"Continue"} />
+            </div>
           </div>
         </form>
       </dialog>
--- client/src/pages/SitterDetailPage.jsx
+++ client/src/pages/SitterDetailPage.jsx
@@ -1,16 +1,14 @@
 import React from "react";
-import SitterReview from "../components/sittedetail/SitterReview";
 import { ImageProvider } from "../contexts/StarRatingContext";
 import SitterDetail from "../components/sittedetail/SitterDetail";
 
 function SitterDetailPage() {
   return (
     <ImageProvider>
-      <div className=" h-screen bg-etc-bg_gray">
+      <div>
         <>
           {/* nav */}
           <SitterDetail/>
-          {/* <SitterReview /> */}
           {/* foot */}
         </>
       </div>

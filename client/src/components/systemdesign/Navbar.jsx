ec53f83b69ba23b64ccd17b3e32e13483edb9436
Nathkit_Nae <nathkit101@gmail.com>
2023-09-05 16:24:17 +0700

    feat:create dropdown list

--- client/src/components/systemdesign/Navbar.jsx
+++ client/src/components/systemdesign/Navbar.jsx
@@ -1,14 +1,82 @@
+import { useContext, useState } from "react";
 import { SitterIconBlack } from "./Icons";
 import { ButtonPrimary } from "./Button";
+import { useAuth } from "../../contexts/authentication";
+import frame2 from "../../assets/SitterReview/frame427320942.png";
+import { UserIcon, PetIcon, ListIcon, LogOutIcon } from "./Icons";
 
-export default function Navbar() {
+function Navbar() {
+  const auth = useAuth();
+
+  const LoginButton = () => {
+    const [hoveredItemId, setHoveredItemId] = useState(null);
+
+    const ListItem = ({ icon: Icon, content, id }) => (
+      <li
+        onMouseEnter={() => setHoveredItemId(id)}
+        onMouseLeave={() => setHoveredItemId(null)}
+        className={
+          hoveredItemId === id
+            ? "hover:text-gray-400 hover:bg-orange-200 hover:rounded-[10px]"
+            : ""
+        }>
+        <a>
+          <Icon
+            color="#3A3B46"
+            hoverColor={hoveredItemId === id ? "#7B7E8F" : "#3A3B46"}
+          />
+          {content}
+        </a>
+      </li>
+    );
+
+    const menuItems = [
+      { icon: UserIcon, content: "Profile" },
+      { icon: PetIcon, content: "Your Pet" },
+      { icon: ListIcon, content: "History" },
+      { icon: LogOutIcon, content: "Log Out" },
+    ];
+
+    if (true) {
+      return (
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0}>
+            <img src={frame2} alt="" className="w-12 h-12" />
+          </label>
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] menu pt-2 shadow bg-etc-white rounded-box w-[186px] text-etc-black text-body2">
+            {menuItems.map((item, idx) => (
+              <ListItem
+                key={idx}
+                id={idx}
+                icon={item.icon}
+                content={item.content}
+              />
+            ))}
+          </ul>
+        </div>
+      );
+    }
+    return (
+      <div className="px-6 py-4 text-body1 text-etc-black hover:text-orange-500">
+        Login
+      </div>
+    );
+  };
+
   return (
     <div className="min-w-[1440px] h-20 px-20 flex justify-between items-center flex-shrink-0">
       <div>
         <SitterIconBlack width="131" height="40" />
       </div>
-      <div className="flex items-center gap-4">
-        <div className="px-6 py-4 text-body1 text-etc-black">Login</div>
+      <div
+        className={
+          auth.isAuthenticate
+            ? "flex items-center gap-6"
+            : "flex items-center gap-4"
+        }>
+        <LoginButton />
         <div>
           <ButtonPrimary content="Find A Pet Sitter" width="168px" />
         </div>
@@ -16,3 +84,5 @@ export default function Navbar() {
     </div>
   );
 }
+
+export default Navbar;

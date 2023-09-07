d7ab5f98db3f23930ffe006912d5f90618219d4a
Nathkit_Nae <nathkit101@gmail.com>
2023-09-07 14:35:16 +0700

    fix bug Navbar not render

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { FacebookIcon } from "../components/systemdesign/Icons";
-import Navbar from "../components/systemdesign/Navbar";
 import Header from "../components/landingpage/Header";
 import FilterBar from "../components/landingpage/FilterBar";
 import Showcase from "../components/landingpage/ShowCase";
 import Banner from "../components/landingpage/Banner";
 import Footer from "../components/systemdesign/Footer";
+import Navbar from "../components/systemdesign/Navbar";
 
 import {
   ButtonPrimary,

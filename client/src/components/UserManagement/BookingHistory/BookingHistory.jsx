4906ca759ec303deac89dee63f77b790afca9ed1
Anthiya Ruchithanatroj <anthiya.r@Anthiya-MacBook-Air.local>
2023-09-11 20:31:54 +0700

    edit: booking history page

--- client/src/components/UserManagement/BookingHistory/BookingHistory.jsx
+++ client/src/components/UserManagement/BookingHistory/BookingHistory.jsx
@@ -1,6 +1,8 @@
 import Avatar from "@mui/material/Avatar";
 import Line from "../../../assets/img/Line.png";
 import BookingHistoryDetail from "./BookingHistoryDetail";
+import { useContext } from "react";
+import { BookingStatusContext } from "../../../contexts/BookingStatusContext";
 
 export const petData = [
   {
@@ -8,7 +10,7 @@ export const petData = [
     trade_name: "Kid friendly",
     pet_sitter_name: "Jane",
     transaction_date: "8/16/2023",
-    status: "Waiting for confirm",
+    status: "Test2",
     date: "3/11/2022",
     starttime: "12 PM",
     endtime: "13 PM",
@@ -70,57 +72,75 @@ export const petData = [
 ];
 
 function BookingHistory() {
+  const { status, setStatus } = useContext(BookingStatusContext);
+  console.log("data", status);
+  const data = status;
+
+  // เอาไว้ใช้ตอนที่ owner / sitter กด button เพื่อเปลี่ยน status
+  const handleClick = (id) => {
+    const updateStatus = data.map((card) => {
+      return card.id === id ? { ...card, status: "Test" } : card;
+    });
+    setStatus(updateStatus);
+  };
+
   return (
     <section className="booking-history flex flex-col gap-6">
-      {petData.map((card) => {
-        return (
-          <div
-            className="booking-history-container border border-gray-200 rounded-2xl p-6"
-            key={card.id}
-            onClick={() =>
-              document.getElementById("booking-detail").showModal()
-            }
-          >
-            <BookingHistoryDetail />
-            <header className="booking-history-header flex justify-between border border-etc-white border-b-gray-200 pb-4">
-              <div className="flex gap-2 items-center">
-                <Avatar alt="avatar" src={card.imageSrc} className="border" />
+      {Array.isArray(data) !== undefined &&
+        data.map((card) => {
+          return (
+            <div
+              className="booking-history-container border border-gray-200 rounded-2xl p-6"
+              key={card.id}
+              onClick={() =>
+                document.getElementById("booking-detail").showModal()
+              }
+            >
+              <BookingHistoryDetail />
+              <header className="booking-history-header flex justify-between border border-etc-white border-b-gray-200 pb-4">
+                <div className="flex gap-2 items-center">
+                  <Avatar alt="avatar" src={card.imageSrc} className="border" />
+                  <div>
+                    <h1 className=" text-headline3">{card.trade_name}</h1>
+                    <h3 className=" text-body1">By {card.pet_sitter_name}</h3>
+                  </div>
+                </div>
                 <div>
-                  <h1 className=" text-headline3">{card.trade_name}</h1>
-                  <h3 className=" text-body1">By {card.pet_sitter_name}</h3>
+                  <p className=" text-body3 text-gray-300 text-right">
+                    Transaction date: {card.transaction_date}
+                  </p>
+                  <h5
+                    className=" text-right text-body2"
+                    onClick={() => handleClick(card.id)}
+                  >
+                    {card.status}
+                  </h5>
                 </div>
-              </div>
-              <div>
-                <p className=" text-body3 text-gray-300 text-right">
-                  Transaction date: {card.transaction_date}
-                </p>
-                <h5 className=" text-right text-body2"> {card.status}</h5>
-              </div>
-            </header>
-            <main className="pt-4 flex gap-x-7 items-center">
-              <div className="text-gray-400 w-1/3 flex flex-col gap-2">
-                <div className=" text-body3">Date & Time:</div>{" "}
-                <div className="text-gray-600 text-body3 ">
-                  {card.date} | {card.starttime} - {card.endtime}
+              </header>
+              <main className="pt-4 flex gap-x-7 items-center">
+                <div className="text-gray-400 w-1/3 flex flex-col gap-2">
+                  <div className=" text-body3">Date & Time:</div>{" "}
+                  <div className="text-gray-600 text-body3 ">
+                    {card.date} | {card.starttime} - {card.endtime}
+                  </div>
                 </div>
+                <img src={Line} alt="line" className="h-9" />
+                <div className="text-gray-400 w-1/3 flex flex-col gap-2">
+                  <div className=" text-body3">Duration:</div>{" "}
+                  <div className="text-gray-600">{card.duration} hours</div>
+                </div>
+                <img src={Line} alt="line" className="h-9" />
+                <div className="text-gray-400 w-1/3 flex flex-col gap-2">
+                  <div className=" text-body3">Pet:</div>
+                  <div className="text-gray-600">{card.pet_name}</div>
+                </div>
+              </main>
+              <div className="text-gray-400 flex-1 w-auto bg-gray-100 p-4 mt-9 rounded-lg">
+                {card.status_message}
               </div>
-              <img src={Line} alt="line" className="h-9" />
-              <div className="text-gray-400 w-1/3 flex flex-col gap-2">
-                <div className=" text-body3">Duration:</div>{" "}
-                <div className="text-gray-600">{card.duration} hours</div>
-              </div>
-              <img src={Line} alt="line" className="h-9" />
-              <div className="text-gray-400 w-1/3 flex flex-col gap-2">
-                <div className=" text-body3">Pet:</div>
-                <div className="text-gray-600">{card.pet_name}</div>
-              </div>
-            </main>
-            <div className="text-gray-400 flex-1 w-auto bg-gray-100 p-4 mt-9 rounded-lg">
-              {card.status_message}
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </section>
   );
 }
--- client/src/pages/UserManagementPage.jsx
+++ client/src/pages/UserManagementPage.jsx
@@ -3,6 +3,8 @@ import { UserIcon, ListIcon, PetIcon } from "../components/systemdesign/Icons";
 import { useState } from "react";
 import Petlist from "../components/UserManagement/Petlist";
 import BookingHistory from "../components/UserManagement/BookingHistory/BookingHistory";
+import { BookingStatusProvider } from "../contexts/BookingStatusContext";
+
 function UserManagementPage() {
   const [userIcon, setUserIcon] = useState(null);
   const [petIcon, setPetIcon] = useState(null);
@@ -10,68 +12,74 @@ function UserManagementPage() {
   const [activeSection, setActiveSection] = useState("profile");
   return (
     <>
-      <Navbar />
-      <div className=" bg-etc-bg_gray w-full px-20 pt-10 pb-20 flex">
-        <div className=" flex flex-col bg-etc-white py-6 w-[292px] h-[289px] rounded-2xl shadow mr-10">
-          <div className="px-6 pb-3 text-headline4">
-            <p className="">Account</p>
+      <BookingStatusProvider>
+        <Navbar />
+        <div className=" bg-etc-bg_gray w-full px-20 pt-10 pb-20 flex">
+          <div className=" flex flex-col bg-etc-white py-6 w-[292px] h-[289px] rounded-2xl shadow mr-10">
+            <div className="px-6 pb-3 text-headline4">
+              <p className="">Account</p>
+            </div>
+            <button
+              className={`px-6 py-5 hover:text-orange-500 text-start focus:bg-orange-100 focus:text-orange-500 text-body1 flex items-center ${
+                activeSection === "profile"
+                  ? "bg-orange-100 text-orange-500"
+                  : ""
+              }`}
+              onClick={() => setActiveSection("profile")}
+              onFocus={() => {
+                setUserIcon("#ff7037");
+              }}
+              onBlur={() => {
+                setUserIcon("#3A3B46");
+              }}
+            >
+              <UserIcon color={userIcon} />
+              <p className="ml-3">Profile</p>
+            </button>
+            <button
+              className={`px-6 py-5 hover:text-orange-500 text-start focus:bg-orange-100 focus:text-orange-500 text-body1 flex items-center ${
+                activeSection === "petlist"
+                  ? "bg-orange-100 text-orange-500"
+                  : ""
+              }`}
+              onClick={() => setActiveSection("petlist")}
+              onFocus={() => {
+                setPetIcon("#ff7037");
+              }}
+              onBlur={() => {
+                setPetIcon("#3A3B46");
+              }}
+            >
+              <PetIcon color={petIcon} />
+              <p className="ml-3">Your Pet</p>
+            </button>
+            <button
+              className={`px-6 py-5 hover:text-orange-500 text-start  focus:text-orange-500 text-body1 flex items-center ${
+                activeSection === "bookingHistory"
+                  ? "bg-orange-100 text-orange-500"
+                  : ""
+              }`}
+              onClick={() => {
+                setActiveSection("bookingHistory");
+              }}
+              onFocus={() => {
+                setListIcon("#ff7037");
+              }}
+              onBlur={() => {
+                setListIcon("#3A3B46");
+              }}
+            >
+              <ListIcon color={listIcon} />
+              <p className="ml-3">Booking History</p>
+            </button>
+          </div>{" "}
+          <div className="p-10 bg-etc-white rounded-2xl w-[956px] h-fit">
+            {activeSection === "profile" && <div>Profile</div>}
+            {activeSection === "petlist" && <Petlist />}
+            {activeSection === "bookingHistory" && <BookingHistory />}
           </div>
-          <button
-            className={`px-6 py-5 hover:text-orange-500 text-start focus:bg-orange-100 focus:text-orange-500 text-body1 flex items-center ${
-              activeSection === "profile" ? "bg-orange-100 text-orange-500" : ""
-            }`}
-            onClick={() => setActiveSection("profile")}
-            onFocus={() => {
-              setUserIcon("#ff7037");
-            }}
-            onBlur={() => {
-              setUserIcon("#3A3B46");
-            }}
-          >
-            <UserIcon color={userIcon} />
-            <p className="ml-3">Profile</p>
-          </button>
-          <button
-            className={`px-6 py-5 hover:text-orange-500 text-start focus:bg-orange-100 focus:text-orange-500 text-body1 flex items-center ${
-              activeSection === "petlist" ? "bg-orange-100 text-orange-500" : ""
-            }`}
-            onClick={() => setActiveSection("petlist")}
-            onFocus={() => {
-              setPetIcon("#ff7037");
-            }}
-            onBlur={() => {
-              setPetIcon("#3A3B46");
-            }}
-          >
-            <PetIcon color={petIcon} />
-            <p className="ml-3">Your Pet</p>
-          </button>
-          <button
-            className={`px-6 py-5 hover:text-orange-500 text-start  focus:text-orange-500 text-body1 flex items-center ${
-              activeSection === "bookingHistory"
-                ? "bg-orange-100 text-orange-500"
-                : ""
-            }`}
-            onClick={() => {
-              setActiveSection("bookingHistory");
-            }}
-            onFocus={() => {
-              setListIcon("#ff7037");
-            }}
-            onBlur={() => {
-              setListIcon("#3A3B46");
-            }}
-          >
-            <ListIcon color={listIcon} />
-            <p className="ml-3">Booking History</p>
-          </button>
-        </div>{" "}
-        <div className="p-10 bg-etc-white rounded-2xl w-[956px] h-fit">
-          {activeSection === "profile" && <div>Profile</div>}
-          {activeSection === "petlist" && <Petlist />}
-          {activeSection === "bookingHistory" && <BookingHistory />}
         </div>
-      </div>
+      </BookingStatusProvider>
     </>
   );
 }

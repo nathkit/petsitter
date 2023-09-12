567fc9daad0b861878d823efdce502431e4e7374
Nathkit_Nae <nathkit101@gmail.com>
2023-09-12 11:40:56 +0700

    add: modal delete button

--- client/src/components/systemdesign/Button.jsx
+++ client/src/components/systemdesign/Button.jsx
@@ -8,8 +8,7 @@ export function ButtonPrimary(props) {
         hover:bg-orange-400 active:bg-orange-600
          disabled:bg-gray-200 disabled:text-gray-300`}
       style={{ width: props.width ? props.width : "139px", height: "48px" }}
-      disabled={props.disabled}
-    >
+      disabled={props.disabled}>
       {props.content}
     </button>
   );
@@ -18,13 +17,13 @@ export function ButtonSecondary(props) {
   return (
     <button
       onClick={props.onClick ? props.onClick : null}
+      type={props.type ? props.type : null}
       className={`inline-flex justify-center items-center py-3 px-6 rounded-full gap-2 border-none
         bg-orange-100 text-orange-500 text-bodyButton
         hover:text-orange-400 active:text-orange-600
          disabled:bg-gray-200 disabled:text-gray-100`}
       style={{ width: props.width ? props.width : "139px", height: "48px" }}
-      disabled={props.disabled}
-    >
+      disabled={props.disabled}>
       {props.content}
     </button>
   );
@@ -33,12 +32,13 @@ export function ButtonGhost(props) {
   return (
     <button
       onClick={props.onClick ? props.onClick : null}
+      type={props.type ? props.type : null}
       className={`inline-flex justify-center items-center py-1 px-0.5 rounded-full gap-1 border-none
       bg-transparent text-orange-500 text-bodyButton
         hover:text-orange-400 active:text-orange-600
          disabled:text-gray-200`}
-      style={{ width: props.width ? props.width : "91px", height: "48px" }}
-    >
+      style={{ width: props.width ? props.width : "91px", height: "48px" }}>
+      {props.icon ? <props.icon /> : null}
       {props.content}
     </button>
   );
@@ -51,8 +51,7 @@ export function ButtonSocial(props) {
         bg-gray-100 text-gray-600 text-bodyButton
        hover:text-gray-400 active:text-gray-600
          disabled:text-gray-600`}
-      style={{ width: props.width ? props.width : "153px", height: "48px" }}
-    >
+      style={{ width: props.width ? props.width : "153px", height: "48px" }}>
       {props.icon ? <props.icon /> : null}
       {props.content}
     </button>
@@ -66,8 +65,7 @@ export function ButtonIcon(props) {
         bg-orange-100 text-orange-500
         hover:text-orange-400 active:text-orange-600
          disabled:bg-gray-200 disabled:text-gray-100`}
-      style={{ width: props.width ? props.width : "60px", height: "60px" }}
-    >
+      style={{ width: props.width ? props.width : "60px", height: "60px" }}>
       {props.icon ? <props.icon /> : null}
     </button>
   );

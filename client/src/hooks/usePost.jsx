25fa386f6a96cc9d6c22a4af8a5bc37cac2ba1ac
Nathkit_Nae <nathkit101@gmail.com>
2023-09-19 17:04:39 +0700

    fix:useAuth in usePost file

--- client/src/hooks/usePost.jsx
+++ client/src/hooks/usePost.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../contexts/authentication";
 
 const usePosts = () => {
   const [petData, setPetData] = useState();

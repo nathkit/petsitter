0fde1d6ad4694ef42d05e6693069de8c047ae5f2
Lilly <korakot.jarunongkran@gmail.com>
2023-09-26 22:06:18 +0700

    feat: pagination in BookingHistory.jsx and edit style in PayoutOption.jsx

--- client/src/components/sitterManagement/PayoutOption.jsx
+++ client/src/components/sitterManagement/PayoutOption.jsx
@@ -28,72 +28,83 @@ function PayoutOption() {
     getSitterPayoutOption();
   }, []);
 
-  const successEntries = payoutOption.filter((entry) => entry.statuses === "Success");
+  const successEntries = payoutOption.filter(
+    (entry) => entry.statuses === "Success"
+  );
 
   return (
-    <section className="flex flex-col h-screen gap-6 p-10 bg-gray-100">
-      <h1 className=" text-headline3 text-[#2A2E3F]">Payout Option</h1>
-      <div className=" w-full flex gap-6 text-body2">
-        <div className=" w-full flex gap-4 p-6 bg-etc-white rounded-2xl text-etc-black">
-          <h3 className=" w-full gap-2 flex flex-row">
-            <BahtIcon /> Total Earning
-          </h3>
-          <div className=" w-auto gap-2 flex flex-row">
-          {successEntries.reduce((total, card) => total + parseFloat(card.amount), 0).toFixed(2)} <h3>THB</h3>
-          </div>
-        </div>
-        <div className=" w-full flex gap-4 p-6 bg-etc-white rounded-2xl ">
-          <h3 className=" w-full gap-2 flex flex-row text-etc-black">
-            <WalletIcon color="#000"/>
-            Bank Account
-          </h3>
-          <div className="w-auto gap-2 flex flex-row text-orange-500">
-            <h3>SCB</h3>
-            <h3>*444</h3>
-            <ArrowRightIcon color="#AEB1C3" />
-          </div>
-        </div>
-      </div>
-      <div>
-        <div className=" flex flex-row w-full bg-etc-black text-etc-white text-body3 rounded-t-2xl">
-          <div className=" w-[240px] px-4 py-3">
-            <h3 className=" w-full h-auto">Date</h3>
-          </div>
-          <div className=" w-[240px] px-4 py-3">
-            <h3 className=" w-full h-auto">From</h3>
-          </div>
-          <div className=" w-[200px] px-4 py-3">
-            <h3 className=" w-full h-auto">Booking No.</h3>
-          </div>
-          <div className=" flex-1 w-full px-4 py-3">
-            <h3 className=" w-full h-auto text-right">Amount</h3>
-          </div>
-        </div>
-        {successEntries && successEntries.length > 0 ? (
-          successEntries.map((card) => (
-            <div
-              className="flex flex-row w-full bg-etc-white border-b border-gray-200 text-body2 text-[#2A2E3F]"
-              key={card.booking_no}
-            >
-              <div className="w-[240px] px-4 py-6">
-                <h3 className="w-full h-auto">{dateTimeFormat(card.booking_date)}</h3>
-              </div>
-              <div className="w-[240px] px-4 py-6">
-                <h3 className="w-full h-auto">{card.full_name}</h3>
-              </div>
-              <div className="w-[200px] px-4 py-6">
-                <h3 className="w-full h-auto">{card.booking_no}</h3>
-              </div>
-              <div className="flex-1 w-full px-4 py-6">
-                <h3 className="w-full h-full text-right text-green-500">
-                  {card.amount} THB
-                </h3>
-              </div>
+    <section className="">
+      <div className="flex flex-col h-full gap-6 p-10 bg-gray-100">
+        <h1 className=" text-headline3 text-[#2A2E3F]">Payout Option</h1>
+        <div className=" w-full flex gap-6 text-body2">
+          <div className=" w-full flex gap-4 p-6 bg-etc-white rounded-2xl text-etc-black">
+            <h3 className=" w-full gap-2 flex flex-row">
+              <BahtIcon /> Total Earning
+            </h3>
+            <div className=" w-auto gap-2 flex flex-row">
+              {successEntries
+                .reduce((total, card) => total + parseFloat(card.amount), 0)
+                .toFixed(2)}{" "}
+              <h3>THB</h3>
             </div>
-          ))
-        ) : (
-          <p className="text-center py-6 bg-etc-white border-b border-gray-200">No successful jobs found🐾</p>
-        )}
+          </div>
+          <div className=" w-full flex gap-4 p-6 bg-etc-white rounded-2xl ">
+            <h3 className=" w-full gap-2 flex flex-row text-etc-black">
+              <WalletIcon color="#000" />
+              Bank Account
+            </h3>
+            <div className="w-auto gap-2 flex flex-row text-orange-500">
+              <h3>SCB</h3>
+              <h3>*444</h3>
+              <ArrowRightIcon color="#AEB1C3" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className=" flex flex-row w-full bg-etc-black text-etc-white text-body3 rounded-t-2xl ">
+            <div className=" w-[240px] px-4 py-3">
+              <h3 className=" w-full h-auto">Date</h3>
+            </div>
+            <div className=" w-[240px] px-4 py-3">
+              <h3 className=" w-full h-auto">From</h3>
+            </div>
+            <div className=" w-[200px] px-4 py-3">
+              <h3 className=" w-full h-auto">Booking No.</h3>
+            </div>
+            <div className=" flex-1 w-full px-4 py-3">
+              <h3 className=" w-full h-auto text-right">Amount</h3>
+            </div>
+          </div>
+          {successEntries && successEntries.length > 0 ? (
+            successEntries.map((card) => (
+              <div
+                className="flex flex-row w-full bg-etc-white border-b border-gray-200 text-body2 text-[#2A2E3F] last:rounded-b-2xl last:border-none "
+                key={card.booking_no}
+              >
+                <div className="w-[240px] px-4 py-6">
+                  <h3 className="w-full h-auto">
+                    {dateTimeFormat(card.booking_date)}
+                  </h3>
+                </div>
+                <div className="w-[240px] px-4 py-6">
+                  <h3 className="w-full h-auto">{card.full_name}</h3>
+                </div>
+                <div className="w-[200px] px-4 py-6">
+                  <h3 className="w-full h-auto">{card.booking_no}</h3>
+                </div>
+                <div className="flex-1 w-full px-4 py-6">
+                  <h3 className="w-full h-full text-right text-green-500">
+                    {card.amount} THB
+                  </h3>
+                </div>
+              </div>
+            ))
+          ) : (
+            <p className="text-center py-6 bg-etc-white border-b border-gray-200">
+              No successful jobs found🐾
+            </p>
+          )}
+        </div>
       </div>
     </section>
   );

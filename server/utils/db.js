2650e8e96d7636afa711e00c117f34083f579748
Nathkit <nathkit101@gmail.com>
2024-10-04 07:41:48 +0700

    Update db.js

--- server/utils/db.js
+++ server/utils/db.js
@@ -4,7 +4,8 @@ const { Pool } = pg.default;
 
 const pool = new Pool({
   connectionString:
-    "postgresql://postgres:vN4Qg4n-ypk@pLS@db.wjxguyrdfqbtwsetylfq.supabase.co:5432/postgres",
+    // "postgresql://postgres:vN4Qg4n-ypk@pLS@db.wjxguyrdfqbtwsetylfq.supabase.co:5432/postgres",
+  "postgresql://postgres.wjxguyrdfqbtwsetylfq:vN4Qg4n-ypk@pLS@aws-0-ap-southeast-1.pooler.supabase.com:6543/postgres",
 });
 
 export default pool;
